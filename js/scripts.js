d805dfdd1b5d8ad305bf397d00da8416c141a08d
Maureen Hidalgo <mchidalgo12@gmail.com>
2020-02-27 21:39:17 -0800

    add if and else if conditional statements for certain combinations

--- js/scripts.js
+++ js/scripts.js
@@ -9,20 +9,20 @@ $(document).ready(function() {
 
 
 
-    if (environment === 'a' && food === 'a' && animal === 'a' || animal === 'a' && task === 'a' && pastime === 'a') {
+    if (environment === 'a' && food === 'a' && animal === 'a' || animal === 'a' && task === 'a' && pastime === 'a' || food === 'a' && task === 'a' && pastime === 'a') {
       $(".person1").text(person1Input);
       $("#python").show();
-    } else if (environment === 'b' && food === 'b' && animal === 'b' || animal === 'b' && task === 'b' && pastime === 'b') {
+    } else if (environment === 'b' && food === 'b' && animal === 'b' || animal === 'b' && task === 'b' && pastime === 'b'|| food === 'b' && task === 'b' && pastime === 'b') {
       $(".person1").text(person1Input);
       $("#ruby").show();
-    } else if (environment === 'c' && food === 'c' && animal === 'c' || animal === 'c' && task === 'c' && pastime === 'c') {
+    } else if (environment === 'c' && food === 'c' && animal === 'c' || animal === 'c' && task === 'c' && pastime === 'c'|| food === 'c' && task === 'c' && pastime === 'c') {
       $(".person1").text(person1Input);
       $("#javascript").show();
-    } else if (environment === 'd' && food === 'd' && animal === 'd' || animal === 'd' && task === 'd' && pastime === 'd') {
+    } else if (environment === 'd' && food === 'd' && animal === 'd' || animal === 'd' && task === 'd' && pastime === 'd'|| food === 'd' && task === 'd' && pastime === 'd') {
       $(".person1").text(person1Input);
       $("#sql").show();
     }
 
     event.preventDefault();
-  }); 
+  });  
 });
